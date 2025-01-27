cf120a83797f7847e2da0e031945fd4b90de6d98
Drian <user@MacBook-Air.local>
2025-01-27 18:03:48 +0700

    pull

--- src/services/apiDashboard.js
+++ src/services/apiDashboard.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const dashboard = async () => {
   try {
     const response = await axios.get(
-      'https://134.209.110.22/api/transaction/summary?filter=week&start_date=2025-01-14'
+      'http://134.209.110.22/api/transaction/summary?filter=week&start_date=2025-01-14'
     );
     return response.data;
   } catch (error) {
