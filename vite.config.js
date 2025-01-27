53ca882a8e9c194ff631dd36374ac73a26cf7caa
Drian <user@MacBook-Air.local>
2025-01-27 19:54:01 +0700

    push

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://134.209.110.22', // Backend URL (insecure HTTP)
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove '/api' prefix if needed
+      },
+    },
+  },
+});
